c2448ae3c352fcef69fdf7a1aefe4e849ab47baa
5bespalk <5bespalk@informatik.uni-hamburg.de>
2018-04-18 15:06:49 +0200

    Aufgabe 1.2.2

--- Draw/src/mydraw/Draw.java
+++ Draw/src/mydraw/Draw.java
@@ -1,29 +1,32 @@
 package mydraw;
 //This example is from _Java Examples in a Nutshell_. (http://www.oreilly.com)
 
-//Copyright (c) 1997 by David Flanagan
-//This example is provided WITHOUT ANY WARRANTY either expressed or implied.
-//You may study, use, modify, and distribute it for non-commercial purposes.
-//For any commercial use, see http://www.davidflanagan.com/javaexamples
-
-// *** modified by PTP 03/2018
-// *** minimal changes from AWT to Swing -> replace elements/classes
-// *** behavior is similiar but not equal ! Why?
-
-import java.applet.*;
-import java.awt.*;
-import java.awt.event.*;
-import java.awt.image.BufferedImage;
-import java.io.File;
+import java.awt.Choice;
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Point;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
-import javax.imageio.ImageIO;
-import javax.swing.*; //++
+//++
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 
 /** The application class. Processes high-level commands sent by GUI */
-/*
- * Zeigt Fenster an. Führt Befehle der Buttons aus Kooperationspartner: DrawGUIs
- */
 public class Draw {
 	/** main entry point. Just create an instance of this application class */
 	public static void main(String[] args) {
@@ -35,7 +38,7 @@ public class Draw {
 		window = new DrawGUIs(this);
 	}
 
-	protected JFrame window; // chg
+	protected DrawGUIs window; // chg
 
 	/** This is the application method that processes commands sent by the GUI */
 	public void doCommand(String command) {
@@ -48,16 +51,234 @@ public class Draw {
 		} else if (command.equals("quit")) { // quit the application
 			window.dispose(); // close the GUI
 			System.exit(0); // and exit.
+		} else if (command.equals("auto")) {
+			autoDraw();
 		}
+	}
+	
+	/**
+	 * Gibt die aktuelle Zeichenfarbe zurück.
+	 * 
+	 * @return aktuelle Zeichenfarbe
+	 */
+	public String getFGColor() {
+		return window.color.toString();
+	}
+
+	/**
+	 * Setzt die aktuelle Zeichenfarbe.
+	 * 
+	 * @param new_color
+	 *            neue Zeichenfarbe
+	 * @throws ColorException
+	 *             ungültige Farbe
+	 */
+	public void setFGColor(String new_color) throws ColorException {
+		switch (new_color.toLowerCase()) {
+		case "black":
+			window.setForeground(Color.black);
+			break;
+		case "green":
+			window.setForeground(Color.GREEN);
+			break;
+		case "red":
+			window.setForeground(Color.RED);
+			break;
+		case "blue":
+			window.setForeground(Color.BLUE);
+			break;
+		default:
+			throw new ColorException();
+		}
+	}
+
+	/**
+	 * Gibt die Breite des Fensters zurück.
+	 * 
+	 * @return Breite des Fensters
+	 */
+	public int getWidth() {
+		return window.getSize().width;
+	}
+
+	/**
+	 * Gibt die Höhe des Fensters zurück.
+	 * 
+	 * @return Höhe des Fensters
+	 */
+	public int getHeight() {
+		return window.getSize().height;
+	}
+
+	/**
+	 * Setzt die Breite des Fensters.
+	 * 
+	 * @param width
+	 *            neue Breite des Fensters
+	 */
+	public void setWidth(int width) {
+		window.setSize(width, getHeight());
+	}
+
+	/**
+	 * Setzt die Höhe des Fensters.
+	 * 
+	 * @param height
+	 *            neue Höhe des Fensters
+	 */
+	public void setHeight(int height) {
+		window.setSize(getWidth(), height);
+	}
+
+	/**
+	 * Setzt die Hintergrundfarbe des Fensters.
+	 * 
+	 * @param new_color
+	 *            neue Hintergrundfarbe
+	 * @throws ColorException
+	 *             ungültige Farbe
+	 */
+	public void setBGColor(String new_color) throws ColorException {
+		Color color;
+		switch (new_color.toLowerCase()) {
+		case "black":
+			color = Color.black;
+			break;
+		case "green":
+			color = Color.green;
+			break;
+		case "red":
+			color = Color.red;
+			break;
+		case "blue":
+			color = Color.blue;
+			break;
+		case "white":
+			color = Color.white;
+			break;
+		default:
+			throw new ColorException();
+		}
+		window.setBackground(color);
+	}
+
+	/**
+	 * Gibt die Hintergrundfarbe des Fensters zurück.
+	 * 
+	 * @return Hintergrundfarbe des Fensters
+	 */
+	public String getBGColor() {
+		return window.getBackground().toString();
+	}
+
+	/**
+	 * Zeichnet ein Recheck.
+	 * 
+	 * @param upper_left
+	 *            linke, obere Ecke
+	 * @param lower_right
+	 *            rechte, untere Ecke
+	 */
+	public void drawRectangle(Point upper_left, Point lower_right) {
+		Graphics g = window.getGraphics();
+		int width = lower_right.x - upper_left.x;
+		int height = lower_right.y - upper_left.y;
+		g.drawRect(upper_left.x, upper_left.y, width, height);
+	}
+
+	/**
+	 * Zeichnet ein Oval.
+	 * 
+	 * @param upper_left
+	 *            linke, obere Ecke
+	 * @param lower_right
+	 *            rechte, untere Ecke
+	 */
+	public void drawOval(Point upper_left, Point lower_right) {
+		Graphics g = window.getGraphics();
+		int width = lower_right.x - upper_left.x;
+		int height = lower_right.y - upper_left.y;
+		g.drawOval(upper_left.x, upper_left.y, width, height);
+	}
+
+	/**
+	 * Zeichnet eine Polylinie
+	 * 
+	 * @param points
+	 *            Liste der Punkte
+	 */
+	public void drawPolyLine(java.util.List<Point> points) {
+		int[] x = new int[points.size()];
+		int[] y = new int[points.size()];
+		for (int i = 0; i < points.size(); i++) {
+			Point p = points.get(i);
+			x[i] = p.x;
+			y[i] = p.y;
+		}
+		Graphics g = window.getGraphics();
+		g.drawPolyline(x, y, points.size());
+	}
+
+	/**
+	 * Gibt die Zeichnung zurück.
+	 * 
+	 * @return Zeichnung
+	 */
+	public Image getDrawing() {
+		return window.createImage(getWidth(), getHeight());
+	}
+
+	/**
+	 * Setzt die Zeichenfläche zurück.
+	 */
+	public void clear() {
+		Graphics g = window.getGraphics();
+		g.setColor(window.getBackground());
+		g.fillRect(0, 0, getWidth(), getHeight());
+	}
+
+	/**
+	 * Zeichnet ein voreingestelltes Bild.
+	 */
+	public void autoDraw() {
+		try {
+			setFGColor("Red");
+			drawRectangle(new Point(100, 100), new Point(300, 300));
+			setFGColor("Blue");
+			drawOval(new Point(50, 50), new Point(200, 200));
+			setFGColor("Green");
+			List<Point> list = Arrays.asList(new Point(50, 50), new Point(100, 150), new Point(80, 80));
+			drawPolyLine(list);
+		} catch (ColorException e) {
+			JOptionPane.showMessageDialog(window, e.getMessage());
+		}
+	}
+
+	/**
+	 * Speichert ein Bild als Datei ab.
+	 * 
+	 * @param img abzuspeicherndes Bild
+	 * @param filename Dateiname
+	 * @throws IOException
+	 */
+	public void writeImage(Image img, String filename) throws IOException {
+		MyBMPFile.write(filename, img);
+	}
+	
+	
+	/**
+	 * Liest eine Bilddatei ein.
+	 * 
+	 * @param filename Dateiname
+	 * @return Image
+	 * @throws IOException
+	 */
+	public Image readImage(String filename) throws IOException {
+		return MyBMPFile.read(filename);
 	}
 }
 
 /** This class implements the GUI for our application */
-/*
- * Definiert das Fenster und ist für die Gestaltung zuständig.
- * Kooperationspartner: JFrame, Draw, DrawActionListener, ShapeManager,
- * ColorItemListener, WindowAdapter
- */
 class DrawGUIs extends JFrame {
 
 	private static final long serialVersionUID = 1L;
@@ -90,6 +311,7 @@ class DrawGUIs extends JFrame {
 		// Create two buttons
 		JButton clear = new JButton("Clear");
 		JButton quit = new JButton("Quit");
+		JButton auto = new JButton("Auto");
 
 		// Set a LayoutManager, and add the choosers and buttons to the window.
 		this.setLayout(new FlowLayout(FlowLayout.RIGHT, 10, 5));
@@ -99,11 +321,9 @@ class DrawGUIs extends JFrame {
 		this.add(color_chooser);
 		this.add(clear);
 		this.add(quit);
+		this.add(auto);
 
 		// Here's a local class used for action listeners for the buttons
-		/*
-		 * Listener für die Buttons. Kooperationspartner: ActionListener
-		 */
 		class DrawActionListener implements ActionListener {
 			private String command;
 
@@ -119,14 +339,10 @@ class DrawGUIs extends JFrame {
 		// Define action listener adapters that connect the buttons to the app
 		clear.addActionListener(new DrawActionListener("clear"));
 		quit.addActionListener(new DrawActionListener("quit"));
+		auto.addActionListener(new DrawActionListener("auto"));
 
 		// this class determines how mouse events are to be interpreted,
 		// depending on the shape mode currently set
-		/*
-		 * Interpretiert die Mausaktionen entsprechend der ausgewählten Zeichenformen.
-		 * Kooperationspartner: ShapeDrawer, ItemListener, DrawGUIs, ScribbleDrawer,
-		 * RectangleDrawer, OvalDrawer
-		 */
 		class ShapeManager implements ItemListener {
 			DrawGUIs gui;
 
@@ -136,9 +352,6 @@ class DrawGUIs extends JFrame {
 			}
 
 			// if this class is active, the mouse is interpreted as a pen
-			/*
-			 * Interpretiert die Mausaktionen als Stift. Kooperationspartner: ShapeDrawer
-			 */
 			class ScribbleDrawer extends ShapeDrawer {
 				int lastx, lasty;
 
@@ -271,10 +484,6 @@ class DrawGUIs extends JFrame {
 
 		shape_chooser.addItemListener(new ShapeManager(this));
 
-		/*
-		 * Setzt die Farbe der neuen Zeichnungen entsprechend der Auswahl.
-		 * Kooperationspartner: ItemListener
-		 */
 		class ColorItemListener implements ItemListener {
 
 			// user selected new color => store new color in DrawGUIs
@@ -307,204 +516,4 @@ class DrawGUIs extends JFrame {
 		this.setVisible(true); // ++
 	}
 
-	/**
-	 * Gibt die aktuelle Zeichenfarbe zurück.
-	 * 
-	 * @return aktuelle Zeichenfarbe
-	 */
-	public String getFGColor() {
-		return color.toString();
-	}
-
-	/**
-	 * Setzt die aktuelle Zeichenfarbe.
-	 * 
-	 * @param new_color
-	 *            neue Zeichenfarbe
-	 * @throws ColorException
-	 *             ungültige Farbe
-	 */
-	public void setFGColor(String new_color) throws ColorException {
-		switch (new_color.toLowerCase()) {
-		case "black":
-			color = Color.black;
-			break;
-		case "green":
-			color = Color.green;
-			break;
-		case "red":
-			color = Color.red;
-			break;
-		case "blue":
-			color = Color.blue;
-			break;
-		default:
-			throw new ColorException();
-		}
-	}
-
-	/**
-	 * Gibt die Breite des Fensters zurück.
-	 * 
-	 * @return Breite des Fensters
-	 */
-	public int getWidth() {
-		return getSize().width;
-	}
-
-	/**
-	 * Gibt die Höhe des Fensters zurück.
-	 * 
-	 * @return Höhe des Fensters
-	 */
-	public int getHeight() {
-		return getSize().height;
-	}
-
-	/**
-	 * Setzt die Breite des Fensters.
-	 * 
-	 * @param width
-	 *            neue Breite des Fensters
-	 */
-	public void setWidth(int width) {
-		setSize(width, getHeight());
-	}
-
-	/**
-	 * Setzt die Höhe des Fensters.
-	 * 
-	 * @param height
-	 *            neue Höhe des Fensters
-	 */
-	public void setHeight(int height) {
-		setSize(getWidth(), height);
-	}
-
-	/**
-	 * Setzt die Hintergrundfarbe des Fensters.
-	 * 
-	 * @param new_color
-	 *            neue Hintergrundfarbe
-	 * @throws ColorException
-	 *             ungültige Farbe
-	 */
-	public void setBGColor(String new_color) throws ColorException {
-		Color color;
-		switch (new_color.toLowerCase()) {
-		case "black":
-			color = Color.black;
-			break;
-		case "green":
-			color = Color.green;
-			break;
-		case "red":
-			color = Color.red;
-			break;
-		case "blue":
-			color = Color.blue;
-			break;
-		case "white":
-			color = Color.white;
-			break;
-		default:
-			throw new ColorException();
-		}
-		setBackground(color);
-	}
-
-	/**
-	 * Gibt die Hintergrundfarbe des Fensters zurück.
-	 * 
-	 * @return Hintergrundfarbe des Fensters
-	 */
-	public String getBGColor() {
-		return getBackground().toString();
-	}
-
-	/**
-	 * Zeichnet ein Recheck.
-	 * 
-	 * @param upper_left
-	 *            linke, obere Ecke
-	 * @param lower_right
-	 *            rechte, untere Ecke
-	 */
-	public void drawRectangle(Point upper_left, Point lower_right) {
-		Graphics g = getGraphics();
-		int width = lower_right.x - upper_left.x;
-		int height = lower_right.y - upper_left.y;
-		g.drawRect(upper_left.x, upper_left.y, width, height);
-	}
-
-	/**
-	 * Zeichnet ein Oval.
-	 * 
-	 * @param upper_left
-	 *            linke, obere Ecke
-	 * @param lower_right
-	 *            rechte, untere Ecke
-	 */
-	public void drawOval(Point upper_left, Point lower_right) {
-		Graphics g = getGraphics();
-		int width = lower_right.x - upper_left.x;
-		int height = lower_right.y - upper_left.y;
-		g.drawOval(upper_left.x, upper_left.y, width, height);
-	}
-
-	/**
-	 * Zeichnet eine Polylinie
-	 * 
-	 * @param points
-	 *            Liste der Punkte
-	 */
-	public void drawPolyLine(java.util.List<Point> points) {
-		int[] x = new int[points.size()];
-		int[] y = new int[points.size()];
-		for (int i = 0; i < points.size(); i++) {
-			Point p = points.get(i);
-			x[i] = p.x;
-			y[i] = p.y;
-		}
-		Graphics g = getGraphics();
-		g.drawPolyline(x, y, points.size());
-	}
-
-	/**
-	 * Gibt die Zeichnung zurück.
-	 * 
-	 * @return Zeichnung
-	 */
-	public Image getDrawing() {
-		return createImage(getWidth(), getHeight());
-	}
-
-	/**
-	 * Setzt die Zeichenfläche zurück.
-	 */
-	public void clear() {
-		Graphics g = getGraphics();
-		g.setColor(getBackground());
-		g.fillRect(0, 0, getWidth(), getHeight());
-	}
-
-	/**
-	 * Zeichnet ein voreingestelltes Bild.
-	 */
-	public void autoDraw() {
-		// TODO autoDraw
-	}
-
-	/**
-	 * Speichert ein Bild als Datei ab.
-	 * 
-	 * @param img
-	 *            abzuspeicherndes Bild
-	 * @param filename
-	 *            Dateiname
-	 * @throws IOException
-	 */
-	public void writeImage(Image img, String filename) throws IOException {
-		// TODO writeImage
-	}
 }
