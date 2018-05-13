3be3fbe27d68ad31e9ec67c6d7b9b71ba69513d3
wyatt <5hoang@informatik.uni-hamburg.de>
2018-05-14 01:27:27 +0200

    - converted Choices to JComboBox - changed Layout

--- Draw/src/mydraw/DrawGUIs.java
+++ Draw/src/mydraw/DrawGUIs.java
@@ -1,10 +1,6 @@
 package mydraw;
 
-import java.awt.BorderLayout;
-import java.awt.Choice;
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.Graphics;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
@@ -14,29 +10,24 @@ import java.awt.event.WindowEvent;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.util.LinkedHashMap;
-import java.util.Map;
+import java.util.*;
 import java.util.Map.Entry;
 
 import javax.imageio.ImageIO;
-import javax.swing.JButton;
-import javax.swing.JFileChooser;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JMenuBar;
-import javax.swing.JPanel;
+import javax.swing.*;
 
 import mydraw.ShapeManager.ShapeDrawer;
 
-/** This class implements the GUI for our application */
-public class DrawGUIs extends JFrame
-{
+/**
+ * This class implements the GUI for our application
+ */
+public class DrawGUIs extends JFrame {
     private BufferedImage bImg;
     private Color color;
     private DrawingPanel panel;
-    private final Map<String, Color> cm = new LinkedHashMap<String, Color>()
-    {
+    private final Map<String, Color> cm = new LinkedHashMap<String, Color>() {
         private static final long serialVersionUID = 1L;
+
         {
             put("black", Color.BLACK);
             put("green", Color.GREEN);
@@ -50,95 +41,99 @@ public class DrawGUIs extends JFrame
     private final Draw app;
     private final int frameWidth = 800;
     private final int frameHeight = 500;
-    private final int menuBarHeight = 40;
-    private final int buttonBarHeight = 40;
+    private final int menuBarHeight = 20;
+    private final int buttonBarHeight = 20;
     private final int panelHeight;
 
     private static final long serialVersionUID = 1L;
 
-	/**
-	 * Constructor.
-	 *
-	 * @param application
-	 *            thid gui's application
-	 */
-	public DrawGUIs(Draw application) {
-		super("Draw");
-		app = application;
-		color = Color.BLACK;
-		panelHeight = frameHeight - menuBarHeight;
-		displayGUI();
-	}
+    /**
+     * Constructor.
+     *
+     * @param application thid gui's application
+     */
+    public DrawGUIs(Draw application) {
+        super("Draw");
+        app = application;
+        color = Color.BLACK;
+        panelHeight = frameHeight - menuBarHeight;
+        displayGUI();
+    }
 
-    public void displayGUI()
-    {
+    public void displayGUI() {
         // Create the Contentpanel.
         panel = new DrawingPanel();
         panel.setBackground(Color.WHITE);
         panel.setPreferredSize(new Dimension(frameWidth, panelHeight));
 
-        // selector for drawing modes
-        final Choice shape_chooser = new Choice();
-        ShapeManager shapeManager = new ShapeManager(panel, this);
-        for (final Entry<String, ShapeDrawer> enp : shapeManager.getDrawerSet())
-        {
-            shape_chooser.add(enp.getKey());
-            System.out.println(enp.getKey());
-        }
-        shape_chooser.addItemListener(shapeManager);
-
-        // selector for drawing colors
-        final Choice color_chooser = new Choice();
-        for (final Entry<String, Color> enp : cm.entrySet())
-        {
-            color_chooser.add(enp.getKey());
-        }
-        color_chooser.addItemListener(new ColorItemListener());
-        
-        // selector for backgroundcolors
-        final Choice bgColor_chooser = new Choice();
-        for (final Entry<String, Color> enp : cm.entrySet())
-        {
-            bgColor_chooser.add(enp.getKey());
-        }
-        bgColor_chooser.addItemListener(new BgColorItemListener());
-        
-
-        // Create buttons
-        final JButton clear = new JButton("Clear");
-        final JButton quit = new JButton("Quit");
-        final JButton auto = new JButton("Auto");
-
         // Create menu bar
-        final JMenuBar mb = new JMenuBar();
+        JMenuBar mb = new JMenuBar();
         mb.setOpaque(true);
         mb.setBackground(Color.lightGray);
         mb.setPreferredSize(new Dimension(frameWidth, menuBarHeight));
-        JButton txtSave = new JButton("Text speichern ...");
-        JButton txtRead = new JButton("Datei lesen...");
-        JButton save = new JButton("Save");
-        mb.add(save, BorderLayout.NORTH);
-        mb.add(txtSave, BorderLayout.NORTH);
-        mb.add(txtRead, BorderLayout.NORTH);
-        
-        // Create the button bar. Make it have a gray background.
-        final JPanel bb = new JPanel();
-        bb.setOpaque(true);
-        bb.setBackground(Color.GRAY);
-        bb.setPreferredSize(new Dimension(frameWidth, buttonBarHeight));
-        
+
+        // Create a menu and add it to the menu bar.
+        JMenu menu = new JMenu("File");
+        mb.add(menu);
+
+        // Create a toolbar.
+        final JToolBar tb = new JToolBar();
+        tb.setOpaque(true);
+        tb.setBackground(Color.GRAY);
+        tb.setPreferredSize(new Dimension(frameWidth, buttonBarHeight));
+
+        // selector for drawing modes
+        ArrayList<String> shape_list = new ArrayList<>();
+        ShapeManager shapeManager = new ShapeManager(panel, this);
+        for (final Entry<String, ShapeDrawer> enp : shapeManager.getDrawerSet()) {
+            shape_list.add(enp.getKey());
+        }
+        String[] shapes = shape_list.toArray(new String[shape_list.size()]);
+        final JComboBox shape_chooser = new JComboBox(shapes);
+        shape_chooser.addItemListener(shapeManager);
+
+        // array of colors
+        ArrayList<String> color_list = new ArrayList<>();
+        for (final Entry<String, Color> enp : cm.entrySet()) {
+            color_list.add(enp.getKey());
+        }
+        String[] colors = color_list.toArray(new String[color_list.size()]);
+
+        // selector for drawing colors
+        final JComboBox color_chooser = new JComboBox(colors);
+        color_chooser.addItemListener(new ColorItemListener());
+
+        // selector for backgroundcolors
+        final JComboBox bgColor_chooser = new JComboBox(colors);
+        bgColor_chooser.addItemListener(new BgColorItemListener());
+
+
+        // Create buttons
+        final JButton clear = new JButton("Clear");
+        final JButton auto = new JButton("Auto");
+
+
+        JMenuItem quit = new JMenuItem("Quit");
+        JMenuItem txtSave = new JMenuItem("Text speichern ...");
+        JMenuItem txtRead = new JMenuItem("Datei lesen...");
+        JMenuItem save = new JMenuItem("Save");
+        menu.add(save);
+        menu.add(txtSave);
+        menu.add(txtRead);
+        menu.add(quit);
+
+
 
         // Set a LayoutManager, and add the choosers and buttons to the button bar.
         // this.setLayout(new FlowLayout(FlowLayout.RIGHT, 10, 5));
-        bb.add(new JLabel("Shape:"));
-        bb.add(shape_chooser);
-        bb.add(new JLabel("Color:"));
-        bb.add(color_chooser);
-        bb.add(new JLabel("Background:"));
-        bb.add(bgColor_chooser);
-        bb.add(clear, BorderLayout.NORTH);
-        bb.add(quit, BorderLayout.NORTH);
-        bb.add(auto, BorderLayout.NORTH);
+        tb.add(new JLabel("Shape:"));
+        tb.add(shape_chooser);
+        tb.add(new JLabel("Color:"));
+        tb.add(color_chooser);
+        tb.add(new JLabel("Background:"));
+        tb.add(bgColor_chooser);
+        tb.add(clear, BorderLayout.SOUTH);
+        tb.add(auto, BorderLayout.NORTH);
 
 
         // Setup BufferedImage
@@ -155,21 +150,19 @@ public class DrawGUIs extends JFrame
         this.setForeground(Color.BLACK);
 
         // Set the menu bar and add the panel to the Content
-        this.getContentPane().add(mb, BorderLayout.NORTH);
-        this.getContentPane().add(bb, BorderLayout.SOUTH);
+        this.setJMenuBar(mb);
+        this.getContentPane().add(tb, BorderLayout.NORTH);
         this.getContentPane()
-            .add(panel, BorderLayout.CENTER);
+                .add(panel, BorderLayout.CENTER);
 
         // Display the window.
         this.pack();
         this.setVisible(true);
 
         // Handle the window close request similarly
-        this.addWindowListener(new WindowAdapter()
-        {
+        this.addWindowListener(new WindowAdapter() {
             @Override
-            public void windowClosing(WindowEvent e)
-            {
+            public void windowClosing(WindowEvent e) {
                 doCommand("quit");
             }
         });
@@ -181,104 +174,84 @@ public class DrawGUIs extends JFrame
         save.addActionListener(new DrawActionListener("save"));
         txtSave.addActionListener(new DrawActionListener("txtSave"));
         txtRead.addActionListener(new DrawActionListener("txtRead"));
-        
+
     }
 
-    public void paintComponent(Graphics g)
-    {
+    public void paintComponent(Graphics g) {
         super.paintComponents(g);
     }
 
-    /** This is the application method that processes commands sent by the GUI */
-    public void doCommand(String command)
-    {
-        if (command.equals("clear"))
-        { // clear the GUI window
-          // It would be more modular to include this functionality in the GUI
-          // class itself. But for demonstration purposes, we do it here.
+    /**
+     * This is the application method that processes commands sent by the GUI
+     */
+    public void doCommand(String command) {
+        if (command.equals("clear")) { // clear the GUI window
+            // It would be more modular to include this functionality in the GUI
+            // class itself. But for demonstration purposes, we do it here.
             app.clear();
-        }
-        else if (command.equals("quit"))
-        { // quit the application
+        } else if (command.equals("quit")) { // quit the application
             this.dispose(); // close the GUI
             System.exit(0); // and exit.
-        }
-
-        else if (command.equals("auto"))
-        {
+        } else if (command.equals("auto")) {
             app.autoDraw();
-        }
-        else if (command.equals("save"))
-        {
-            try
-            {
+        } else if (command.equals("save")) {
+            try {
 //                app.writeImage(bImg, "mybImg.png");
                 JFileChooser jfc = new JFileChooser();
                 int retVal = jfc.showSaveDialog(null);
-                if(retVal==JFileChooser.APPROVE_OPTION){
+                if (retVal == JFileChooser.APPROVE_OPTION) {
                     File f = jfc.getSelectedFile();
                     String test = f.getAbsolutePath();
-                    ImageIO.write((BufferedImage)bImg,"png",new File(test));
-                 }
-            }
-            catch (final IOException e)
-            {
+                    ImageIO.write((BufferedImage) bImg, "png", new File(test));
+                }
+            } catch (final IOException e) {
                 e.printStackTrace();
             }
-        }
-        else if (command.equals("txtSave"))
-        {
-            try
-            {
+        } else if (command.equals("txtSave")) {
+            try {
                 app.writeText("commands");
-            }
-            catch (TxtIOException e)
-            {
+            } catch (TxtIOException e) {
                 // TODO Auto-generated catch block
                 e.printStackTrace();
             }
-        }
-        else if(command.equals("txtRead"))
-        {
-            
+        } else if (command.equals("txtRead")) {
+
         }
     }
 
-	/**
-	 * Returns the current drawing color.
-	 *
-	 * @return drawing color
-	 */
-	public Color getColor() {
-		return color;
-	}
+    /**
+     * Returns the current drawing color.
+     *
+     * @return drawing color
+     */
+    public Color getColor() {
+        return color;
+    }
 
-	/**
-	 * Sets the new drawing color.
-	 *
-	 * @param newColor
-	 *            new drawing color
-	 */
-	public void setColor(Color newColor) {
-		color = newColor;
-	}
+    /**
+     * Sets the new drawing color.
+     *
+     * @param newColor new drawing color
+     */
+    public void setColor(Color newColor) {
+        color = newColor;
+    }
 
-	/**
-	 * Returns the color map with all available colors.
-	 *
-	 * @return color map
-	 */
-	public Map<String, Color> getColorMap() {
-		return cm;
-	}
+    /**
+     * Returns the color map with all available colors.
+     *
+     * @return color map
+     */
+    public Map<String, Color> getColorMap() {
+        return cm;
+    }
 
     /**
      * Returns the drawing panel.
      *
      * @return panel
      */
-    public JPanel getDrawingPanel()
-    {
+    public JPanel getDrawingPanel() {
         return panel;
     }
 
@@ -287,50 +260,41 @@ public class DrawGUIs extends JFrame
      *
      * @return buffered image
      */
-    public BufferedImage getBufferedImage()
-    {
+    public BufferedImage getBufferedImage() {
         return bImg;
     }
 
     // Here's a local class used for action listeners for the buttons
-    class DrawActionListener implements ActionListener
-    {
+    class DrawActionListener implements ActionListener {
         private final String command;
 
-        public DrawActionListener(String cmd)
-        {
+        public DrawActionListener(String cmd) {
             command = cmd;
         }
 
         @Override
-        public void actionPerformed(ActionEvent e)
-        {
+        public void actionPerformed(ActionEvent e) {
             doCommand(command);
         }
     }
 
-    class ColorItemListener implements ItemListener
-    {
+    class ColorItemListener implements ItemListener {
         // user selected new color => store new color in DrawGUIs
         @Override
-        public void itemStateChanged(ItemEvent e)
-        {
+        public void itemStateChanged(ItemEvent e) {
             final Color newColor = cm.get(e.getItem());
-            if (newColor != null)
-            {
+            if (newColor != null) {
                 color = cm.get(e.getItem());
             }
         }
     }
-    class BgColorItemListener implements ItemListener
-    {
+
+    class BgColorItemListener implements ItemListener {
         // user selected new color => store new color in DrawGUIs
         @Override
-        public void itemStateChanged(ItemEvent e)
-        {
+        public void itemStateChanged(ItemEvent e) {
             final Color newColor = cm.get(e.getItem());
-            if (newColor != null)
-            {
+            if (newColor != null) {
                 panel.setBackground(newColor);
             }
         }
